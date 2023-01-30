42cbcd72e99cc2d64237b2ad378c303fd6e9c987
psysh12 <psysh12@exmail.nottingham.ac.uk>
2023-01-30 22:00:48 +0000

    first commit

---
+++ main.java
@@ -0,0 +1,16 @@
+class Print
+{
+    public static void main(String[] args) {
+        helloWorld();
+        String msg = "message.";
+        print(msg);
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+}
